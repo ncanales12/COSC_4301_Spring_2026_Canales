da8696c7695f36985583316d264c11051d92a43f
ncanales12 <nicolas.canales@g.austincc.edu>
2026-03-21 22:53:17 -0400

    Project builds, database migrations working

---
+++ monsterbountyserver/src/main/java/org/example/monsterbountyserver/MonsterbountyserverApplication.java
@@ -0,0 +1,13 @@
+package org.example.monsterbountyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsterbountyserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonsterbountyserverApplication.class, args);
+	}
+
+}
